fdb1b807bc7639d85cb00bfe47129dd90f8c6317
Nathan <antnefedov33@gmail.com>
2025-05-14 22:27:02 +0300

    gulp file updated

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,146 +1,139 @@
-/* DEPENDENCIES */
-import browserSync, { init, notify, stream } from 'browser-sync'; // inject code to all devices
-import gulp from 'gulp'; // gulp core
-import autoprefixer from 'gulp-autoprefixer'; // sets missing browser prefixes
-import clean from 'gulp-clean'; // remove files and folders
-import cleanCSS from 'gulp-clean-css'; // minify CSS files
-import htmlmin from 'gulp-htmlmin'; // minify HTML files
-import gulpif from 'gulp-if'; // conditionally run a task
-import imagemin from 'gulp-imagemin'; // minify images
+import browserSync, { init, notify, stream } from 'browser-sync';
+import gulp from 'gulp';
+import autoprefixer from 'gulp-autoprefixer';
+import clean from 'gulp-clean';
+import cleanCSS from 'gulp-clean-css';
+import htmlmin from 'gulp-htmlmin';
+import gulpif from 'gulp-if';
 import * as sass from 'sass';
-import gulpSass from 'gulp-sass'; // sass compiler
-import terser from 'gulp-terser'; // minify JavaScript files
-import useref from 'gulp-useref'; // parse build blocks in HTML files
-import spritesmith from 'gulp.spritesmith'; // create sprites
-import pngquant from 'imagemin-pngquant'; // minify png-format images
-import copy from 'gulp-copy'; // simple file copy plugin
+import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
+import useref from 'gulp-useref';
+import spritesmith from 'gulp.spritesmith';
+import fs from 'fs';
+import copy from 'gulp-copy';
 
 const { series, parallel, src, dest, watch } = gulp;
 const sassCompiler = gulpSass(sass);
 
-/* BROWSERSYNC (LOCAL SERVER) */
+function directoryExists(dirPath) {
+	try {
+		return fs.existsSync(dirPath) && fs.readdirSync(dirPath).length > 0;
+	} catch (err) {
+		return false;
+	}
+}
+
 function serve() {
 	browserSync.init({
 		server: {
 			baseDir: './src/',
-		}, // base dir
-		notify: false, // disable notification
-		scrollRestoration: true, // save scroll position
+		},
+		notify: false,
+		scrollRestoration: true,
 	});
 }
 
-/* WATCHER (WATCHING FILE CHANGES)*/
 function watcher() {
 	watch('./src/**/*.html', html);
 	watch('./src/scss/**/*.+(scss|sass|css)', compileSass);
 	watch('./src/js/**/*.js', scripts);
-	watch('./src/img/sprite/*.*', sprite);
+	watch('./src/assets/img/sprite/*.*', sprite);
 }
 
-/* HTML */
 function html() {
-	return src('./src/index.html') // get the files
-		.pipe(dest('./build/')) // where to put the file
-		.pipe(browserSync.stream()); // browsersync stream
+	return src('./src/index.html', {
+		allowEmpty: true,
+		buffer: true,
+		sourcemaps: true,
+	}).pipe(browserSync.stream());
 }
 
-/* SASS */
 function compileSass() {
-	return src('./src/scss/**/*') // get the files
-		.pipe(sassCompiler().on('error', sassCompiler.logError)) // add prefixes
+	return src('./src/scss/**/*.scss', { allowEmpty: true })
 		.pipe(
-			autoprefixer({
-				overrideBrowserslist: ['last 2 versions'],
-				cascade: true,
-			})
+			sassCompiler({ outputStyle: 'expanded' }).on(
+				'error',
+				sassCompiler.logError
+			)
 		)
-		.pipe(dest('src/css')) // where to put the file
-		.pipe(browserSync.stream()); // browsersync stream
+		.pipe(
+			gulpif(
+				process.env.NODE_ENV === 'production',
+				autoprefixer({
+					overrideBrowserslist: ['last 2 versions'],
+					cascade: true,
+				})
+			)
+		)
+		.pipe(dest('src/css'))
+		.pipe(browserSync.stream());
 }
 
-/* JS */
 function scripts() {
-	return src('./src/js/**/*.js') // get the files
-		.pipe(browserSync.stream()); // browsersync stream
+	if (process.env.NODE_ENV === 'production') {
+		return Promise.resolve();
+	}
+	return src('./src/js/**/*.js').pipe(browserSync.stream());
 }
 
-/* IMAGES */
 function sprite(done) {
-	buildSprite().on('end', done);
+	if (directoryExists('./src/assets/img/sprite')) {
+		buildSprite().on('end', done);
+	} else {
+		done();
+	}
 }
 
-function images() {
-	return src('./src/assets/img/**/*') // get the files
-		.pipe(
-			imagemin({
-				// minify images
-				progressive: true,
-				svgoPlugins: [{ removeViewBox: false }, { cleanupIDs: false }],
-				use: [
-					pngquant({
-						// minify png-format images
-						quality: [0.5, 0.7],
-						speed: 4,
-					}),
-				],
-				interlaced: true,
-			})
-		)
-		.pipe(dest('build/assets/img')); // where to put the files
+function copyImages() {
+	if (directoryExists('./src/assets/img')) {
+		return src('./src/assets/img/**/*').pipe(copy('./build', { prefix: 1 }));
+	}
+	return Promise.resolve();
 }
 
-/* FAVICON */
-function favicon() {
-	return src('./src/assets/icons/favicon.ico') // get the favicon
-		.pipe(copy('build/assets/icons', { prefix: 4 })); // copy to destination
+function copyFonts() {
+	if (directoryExists('./src/assets/fonts')) {
+		return src('./src/assets/fonts/**/*').pipe(copy('./build', { prefix: 1 }));
+	}
+	return Promise.resolve();
 }
 
-/* DEMO */
-function demo() {
-	return src('./src/assets/demo/**/*') // get all files in the demo folder
-		.pipe(dest('build/assets/demo')); // copy to destination folder
+function copyIcons() {
+	if (directoryExists('./src/assets/icons')) {
+		return src('./src/assets/icons/**/*').pipe(copy('./build', { prefix: 1 }));
+	}
+	return Promise.resolve();
 }
 
-/* SCREENSHOTS */
-function screenshots() {
-	return src('./src/assets/screenshots/**/*') // get all files in the screenshots folder
-		.pipe(dest('build/assets/screenshots')); // copy to destination folder
+function copyLibs() {
+	if (directoryExists('./src/js/libs')) {
+		return src('./src/js/libs/**/*').pipe(copy('./build', { prefix: 1 }));
+	}
+	return Promise.resolve();
 }
 
-/* FONTS */
-function fonts() {
-	return src('./src/assets/fonts/**/*') // get the files
-		.pipe(dest('build/assets/fonts')); // where to put the files
-}
-
-/* LIBS (PERSONAL DEVELOPER LIBS) */
-function libs() {
-	return src('./src/assets/js-libs/**/*') // get the files
-		.pipe(dest('build/libs')); // where to put the files
-}
-
-/* EXTRAS (ROOT FILES, EXCEPT HTML) */
-function extras() {
-	return src(['src/*.*', '!src/*.html']) // get the files
-		.pipe(dest('build')); // where to put the files
-}
-
-/* CLEAN */
 function cleanDist() {
-	return src('build', { read: false, allowEmpty: true }).pipe(clean()); // clean dir
+	return src('./build', { allowEmpty: true }).pipe(clean());
 }
 
 function build() {
-	const assets = useref();
 	return src('src/*.html')
-		.pipe(assets)
-		.pipe(gulpif('*.js', terser())) // minify JS
-		.pipe(gulpif('*.css', cleanCSS())) // minify CSS
-		.pipe(gulpif('*.html', htmlmin({ collapseWhitespace: true }))) // minify HTML
-		.pipe(dest('./build')); // where to put the files
+		.pipe(useref())
+		.pipe(gulpif('*.js', terser()))
+		.pipe(gulpif('*.css', cleanCSS()))
+		.pipe(
+			gulpif(
+				'*.html',
+				htmlmin({
+					collapseWhitespace: true,
+					removeComments: true,
+				})
+			)
+		)
+		.pipe(dest('./build'));
 }
 
-/* FUNCTIONS */
 function buildSprite() {
 	const spriteData = src('./src/assets/img/sprite/*.*').pipe(
 		spritesmith({
@@ -155,26 +148,14 @@ function buildSprite() {
 	return spriteData.css.pipe(dest('./src/scss/components'));
 }
 
-/* TASK EXPORTS */
 export const dev = series(
-	cleanDist,
-	parallel(html, compileSass, scripts),
+	parallel(html, compileSass),
 	parallel(serve, watcher)
 );
 
 export const prod = series(
 	cleanDist,
-	parallel(
-		html,
-		compileSass,
-		scripts,
-		images,
-		favicon,
-		demo,
-		screenshots,
-		fonts,
-		libs,
-		extras
-	),
+	compileSass,
+	parallel(scripts, copyImages, copyFonts, copyIcons, copyLibs),
 	build
 );
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,4 @@
-('use strict');
+'use strict';
 
 const squares = document.querySelector('.squares');
 let monitorWidth = window.innerWidth;
